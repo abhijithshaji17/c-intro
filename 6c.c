a33dc6705bdd095997c03729a5963a55f7ba6ae5
abhijithshaji17 <abhijithshaji700@gmail.com>
2026-01-08 18:55:30 +0530

    resources ready

---
+++ 6c.c
@@ -0,0 +1,11 @@
+// using do while loop
+#include<stdio.h>
+int main() {
+    int i = 1;
+    do {
+        printf("TinkerHub is Cool!\n");
+        i++; 
+    } while (i <= 3); // Change 5 to 100 if you want it 100 times
+    
+    return 0;
+}
